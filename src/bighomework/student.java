e4ba1cdd859fc9124b17c8e49f6772889a84b93a
kitakita <110728627+kitakitasu@users.noreply.github.com>
2024-07-16 21:47:34 +0800

    Update student.java

--- src/bighomework/student.java
+++ src/bighomework/student.java
@@ -1,1 +1,44 @@
+package bighomework;
 
+public class student<E,T,V> extends persons<E,T,V> implements thans<E,T,V>{
+    private String sno;
+    private String grade;
+
+    public student() {
+    }
+
+    public student(String sno, String grade) {
+        this.sno = sno;
+        this.grade = grade;
+    }
+
+    public student(String name, String sex, T age, E born, String sno, String grade) {
+        super(name, sex, age, born);
+        this.sno = sno;
+        this.grade = grade;
+    }
+
+    public String getSno() {
+        return sno;
+    }
+
+    public void setSno(String sno) {
+        this.sno = sno;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString()+", " +
+                "grade='" + grade + '\'' +
+                ", sno='" + sno + '\'' +
+                "} " ;
+    }
+}
